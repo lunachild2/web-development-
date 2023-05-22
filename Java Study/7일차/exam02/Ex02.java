72e8c2ad3383116d86c14f413599fac4c3616e1e
lunachild2 <103565090+lunachild2@users.noreply.github.com>
2023-05-22 16:15:30 +0900

    접근제어자

---
+++ Java Study/7일차/exam02/Ex02.java
@@ -0,0 +1,12 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        A a = new A();
+        System.out.println(a.num1);
+
+
+
+    }
+}
---
+++ Java Study/7일차/exam02/sub/B.java
@@ -0,0 +1,12 @@
+package exam02.sub;
+
+import exam02.A;
+
+public class B extends A {
+
+    public void method() {
+        num4 = 50;
+    }
+
+
+}
