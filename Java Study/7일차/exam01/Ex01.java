294aa6dea5d3359451ccbc231bb728b506b67bb3
lunachild2 <103565090+lunachild2@users.noreply.github.com>
2023-05-22 16:08:52 +0900

    Add files via upload

---
+++ Java Study/7일차/exam01/Ex01.java
@@ -0,0 +1,27 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        Schedule s1 = new Schedule();
+
+        // 멤버변수에 값을 대입 -> 통제 불가 -> 직접 값을 대입을 지양
+        // 멤버변수의 접근 범위 private
+        // 멤버변수를 메서드를 통해서 값을 지정 set
+        // 멤버변수를 메서드를 통해서 값을 조회 get...
+        /*
+        s1.year = 2023;
+        s1.month = 2;
+        s1.day = 31;
+        */
+        s1.setYear(2023);
+        s1.setMonth(2);
+        s1.setDay(31);
+
+
+
+        s1.showDate();
+
+
+    }
+}
---
+++ Java Study/7일차/exam01/Ex02.java
@@ -0,0 +1,13 @@
+package exam01;
+
+import exam02.A;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        A a = new A();
+
+
+
+    }
+}
