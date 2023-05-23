205d7971aead885cbe01033a5df41c99ddf8398d
lunachild2 <103565090+lunachild2@users.noreply.github.com>
2023-05-23 16:41:38 +0900

    Add files via upload

---
+++ Java Study/8일차/exam02/Ex02.java
@@ -0,0 +1,16 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Student.id = 1000;
+        System.out.println(Student.id);
+
+        Student.staticMethod();
+
+        Class cls = Student.class;
+
+
+
+    }
+}
